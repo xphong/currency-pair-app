a41a99d6106360ebf78219d6807786db902facc7
Phong Huynh <xphong@gmail.com>
2018-04-11 21:28:36 -0400

    Add html and webpack dev server
    - Create index.html
    - Use webpack-dev-server to serve application
    - Add HtmlWebPackPlugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello World</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));

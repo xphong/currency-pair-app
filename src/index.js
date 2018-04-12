0795836aa3f75e16457abd7cc4f6de61e43b7772
Phong Huynh <xphong@gmail.com>
2018-04-12 19:22:07 -0400

    Set up redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import configureStore from './store/configureStore';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);

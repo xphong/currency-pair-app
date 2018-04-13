a025483c5a6458c1d02905c28e80fcf205a3af59
Phong Huynh <xphong@gmail.com>
2018-04-12 20:01:30 -0400

    Add redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+export default function configureStore( initialState = {}) {
+  const middleware = [thunk];
+
+  let enhancer = applyMiddleware(...middleware);
+
+  if (process.env.NODE_ENV !== 'production') {
+    enhancer = composeWithDevTools(enhancer);
+  }
+
+  return createStore(rootReducer, initialState, enhancer);
 }

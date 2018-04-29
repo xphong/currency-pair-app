ca555dde0122940235d177730ed1c5c3a3c682d5
Phong Huynh <xphong@gmail.com>
2018-04-29 10:01:31 -0400

    Update spinner component to include inline type

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Spinner.css';
 
-export const Spinner = () => {
-  return <div className={styles.loading}>Loading&#8230;</div>;
+export const Spinner = ({ type }) => {
+  const loadingClassName = styles[`loading${type}`];
+  return <div className={loadingClassName}>Loading&#8230;</div>;
 };
+
+Spinner.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+Spinner.defaultProps = {
+  type: 'Full'
+};
+
 
 export default Spinner;
